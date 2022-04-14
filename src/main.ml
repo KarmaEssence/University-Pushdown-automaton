6a5f483f3957a1193ce1cb069b9eabcaf666abb1
leo <karmaessence@outlook.fr>
2022-04-14 19:38:19 +0200

    clean ast file

--- src/main.ml
+++ src/main.ml
@@ -2,4 +2,4 @@ let lexbuf = Lexing.from_channel stdin
 
 let ast = Parser.input Lexer.main lexbuf 
 
-let _ = Ast.print_automate ast
+let _ = Print_ast.print_automate ast
---
+++ src/print_ast.ml
@@ -0,0 +1,44 @@
+open Ast
+
+let rec print_stringlist (list : string list) (flag : int): unit =
+  match list with 
+  | [] -> 
+    if flag == 0 then
+      print_string "\n"
+    else
+      print_string ""  
+  | element :: sublist -> 
+      print_string (element ^ " ");
+      print_stringlist sublist flag
+  ;;  
+
+let rec print_transitions (transitions: transition list) : unit = 
+  match transitions with
+  | [] -> print_string "\n"
+  | (a, b, c, d, e) :: sublist ->
+    print_string (a ^ " ");
+    print_stringlist b 1;
+    print_string (c ^ " " ^ d ^ " ");
+    print_stringlist e 0;
+    print_transitions sublist
+  ;;  
+
+let print_declarations (declarations: declarations) : unit = 
+  match declarations with
+  | (input_symbols, stack_symbols, states, initial_state, initial_stack) ->
+    print_string "Input symbols : ";
+    print_stringlist input_symbols 0; 
+    print_string "Stack symbols : ";
+    print_stringlist stack_symbols 0;
+    print_string "States : ";
+    print_stringlist states 0;
+    print_string ("Initial symbols : " ^ initial_state ^ "\n");
+    print_string ("Initial stack : " ^ initial_stack ^ "\n")
+
+  ;;  
+
+let print_automate (ast: automate) : unit =
+  match ast with
+  | (declarations, transitions) ->
+    print_declarations declarations;
+    print_transitions transitions
