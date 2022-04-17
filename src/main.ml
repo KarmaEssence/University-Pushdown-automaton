ea6e2103718d1b4f835ce9585e585d5ddac27df3
leo <karmaessence@outlook.fr>
2022-04-17 21:01:55 +0200

    update 11

--- src/main.ml
+++ src/main.ml
@@ -1,16 +1,24 @@
-let lexbuf = Lexing.from_channel stdin 
+open Ast
 
-let ast = Parser.input Lexer.main lexbuf
+let open_automaton_file (filename: string) : automate = 
+  let lexbuf = Lexing.from_channel (open_in filename) in
+  let automaton = Parser.input Lexer.main lexbuf in 
+  Check_ast.check_automate automaton;
+  automaton
 
 let usage () =
   print_string "\n";
   print_string "Options d'exécution du programme : \n";
-  print_string "./parser < file -evaluation <mot> : évalutation de l'automate à l'aide d'un mot\n";
-  print_string "./parser < file -print : affichage de l'automate dans le terminal\n";
+  print_string "./main -eval <file> <mot> : évalutation de l'automate à l'aide d'un mot\n";
+  print_string "./main -print <file> : affichage de l'automate dans le terminal\n";
   print_string "\n"
 
 let _ =
   match Sys.argv with
-  | [|_;"-eval";argument|] -> Check_ast.check_automate ast; Eval_ast.eval_automate ast argument
-  | [|_;"-print"|] -> Check_ast.check_automate ast; Print_ast.print_automate ast   
+  | [|_;"-eval";filename;argument|] ->
+    let automate = open_automaton_file filename in
+    Eval_ast.eval_automate automate argument
+  | [|_;"-print";filename|] ->  
+    let automate = open_automaton_file filename in
+    Print_ast.print_automate automate  
   | _ -> usage ()
