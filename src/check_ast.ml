83f8adb79a71951f363619cd160b41223f4c5ac0
leo <karmaessence@outlook.fr>
2022-05-21 16:15:24 +0200

    fix bug

--- src/check_ast.ml
+++ src/check_ast.ml
@@ -8,19 +8,44 @@ open Type
 (*                              check_ast                              *)  
 (***********************************************************************)
 
+let rec check_list (transition_to_read: automate_transition) (transitions: automate_transition list): bool =
+  match transition_to_read with
+  | (x_current_state, x_listletter_toread, x_stack_topop, _, _) ->
+    match transitions with
+    | [] -> true
+    | (y_current_state, y_listletter_toread, y_stack_topop, _, _) :: subtransitions ->
+      if x_current_state = y_current_state && x_stack_topop = y_stack_topop 
+        && ((List.length x_listletter_toread = 0 && List.length y_listletter_toread > 0) ||
+            (List.length y_listletter_toread = 0 && List.length x_listletter_toread > 0)) then
+        false      
+      else      
+        check_list transition_to_read subtransitions
+
+let rec check_transitions_in_case_of_epsilon (transitions_to_read: automate_transition list) 
+(transitions: automate_transition list): unit = 
+  match transitions_to_read with
+  | [] -> ()
+  | transition :: subtransitions_to_read ->
+    if check_list transition transitions then
+      check_transitions_in_case_of_epsilon subtransitions_to_read transitions
+    else
+      let error_code = 1 in
+      print_string "Error: The product automate must be deterministic.\n";
+      exit error_code                     
+
 (*Allow to verify if transitions have good format, else print accurately the error
 and exit the program.*)
 let rec check_transitions (transitions: automate_transition list) (map: string list NameTable.t): unit = 
   match transitions with
-  | [] -> print_string "\n"
+  | [] -> ()
   | transition :: subtransitions ->
     match transition with
     | (current_state, listletter_toread, stack_topop, wanted_state, list_stack_topush) ->
-      let value = (Char.escaped current_state) ^ (Char.escaped wanted_state) in
+      let value = (Char.escaped current_state) ^ (Char.escaped wanted_state) ^ (Char.escaped stack_topop) in
       if List.length listletter_toread > 0 && (NameTable.mem (List.hd listletter_toread) map) then
         let key = List.hd listletter_toread in
         let list_of_word = NameTable.find key map in
-        if List.for_all (fun x-> (String.sub x 0 1) = (String.sub value 0 1) && x <> value) list_of_word then
+        if List.exists (fun x-> (String.sub x 0 1) = (String.sub value 0 1) && (String.sub x 1 1) <> (String.sub value 1 1)) list_of_word then
           let error_code = 1 in
           print_string "Error: The product automate must be deterministic.\n";
           exit error_code
@@ -62,5 +87,6 @@ let check_automate (automate: automate): unit =
   | Automate(declarations, transitions) ->
     check_declarations declarations;
     let map = NameTable.empty in
-    check_transitions transitions map 
+    check_transitions transitions map; 
+    check_transitions_in_case_of_epsilon transitions transitions
   | Program(_,_) -> ()
